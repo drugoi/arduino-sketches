4d54cd66821ed85972f0e0bed8c9b4b0652cfb11
Nikita Bayev <drugoi.nik@gmail.com>
2016-04-25 21:57:26 +0600

    Added some conditions for TempToLCD programm

--- temperaturetolcd/temperaturetolcd.ino
+++ temperaturetolcd/temperaturetolcd.ino
@@ -18,33 +18,37 @@ const int B = 3975;
 void setup()
 {
 
-    lcd.begin(16, 2);
-    lcd.setRGB(colorR, colorG, colorB);
-    // Configure the serial communication line at 9600 baud (bits per second.)
-    Serial.begin(9600);
+  lcd.begin(16, 2);
+  // Configure the serial communication line at 9600 baud (bits per second.)
+  Serial.begin(9600);
 }
 
 void loop()
 {
-    // Get the (raw) value of the temperature sensor.
-    int val = analogRead(pinTemp);
+  // Get the (raw) value of the temperature sensor.
+  int val = analogRead(pinTemp);
 
-    // Determine the current resistance of the thermistor based on the sensor value.
-    float resistance = (float)(1023-val)*10000/val;
+  // Determine the current resistance of the thermistor based on the sensor value.
+  float resistance = (float)(1023 - val) * 10000 / val;
 
-    // Calculate the temperature based on the resistance value.
-    float temperature = 1/(log(resistance/10000)/B+1/298.15)-273.15;
+  // Calculate the temperature based on the resistance value.
+  float temperature = 1 / (log(resistance / 10000) / B + 1 / 298.15) - 273.15;
 
-    // Print the temperature to the serial console.
-    // Serial.println(temperature);
+  // Print the temperature to the LCD display.
+  
+  lcd.begin(16, 2);
 
-    lcd.begin(16, 2);
-    
+  if (temperature >= 30) {
+    lcd.setRGB(255, 0, 0);
+  } else if (temperature <= 20) {
+    lcd.setRGB(0, 128, 255);
+  } else {
+    lcd.setRGB(0, 255, 0);
+  }
 
-    lcd.print(temperature);
+  lcd.print(temperature);
 
-    // Wait one second between measurements.
-    delay(1000);
-    lcd.clear();
+  delay(5000);
+  lcd.clear();
 }
 
